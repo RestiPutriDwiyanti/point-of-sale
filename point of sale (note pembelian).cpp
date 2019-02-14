dbb47eae4b00179425bc43211643c739fc6fad33
RestiPutriDwiyanti <47617404+RestiPutriDwiyanti@users.noreply.github.com>
2019-02-14 09:06:15 +0700

    Create point of sale (note pembelian).cpp
    untuk note pembelian IT service

---
+++ point of sale (note pembelian).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdlib.h>
+#include<conio.h>
+#include <string> 
+   using namespace std;
+   int main() { 
+      int no,tgl,ns;
+      string jl,JL,Jl,tanggal,kl,ps,nm,name,nama,telp;
+   
+   
+  
+  									cout<<"IT Service "<<endl;
+  								cout<<"ACP SMKN $ PADALARANG "<<endl;
+  								cout<<"Jl. Raya Padalarang 451"<<endl;
+				cout<<"=========================================================="<<endl;
+cout<<"No =";		
+cin>>no;									
+cout<<"Jenis Laptop ="; 
+cin>>jl>>JL>>Jl; 
+cout<<"Tanggal =";
+ cin>>tgl>>tanggal; 
+ cout<<"nama =";
+ cin>>nm>>name;
+ cout<<"No.seri=";
+ cin>>ns;
+ cout<<"Telepon=";
+ cin>>telp;
+ 	
+
+	
+	cout<<" From Keluhan"<<endl;
+			cout<<"Keluhan Laptop ="<<endl;
+			cin>>kl;
+			cout<<"Perkiraan Selesai ="<<endl;
+			cin>>ps;
+			
+			system ("PAUSE");
+			return 0;
+}
